8388567a93063d882a44c3e780817ee13ae57171
Ralph-Gordon Paul <ralph-gordon.paul@uni-duesseldorf.de>
2014-04-19 19:20:10 +0200

    added first implementation

---
+++ RGP-Log.cpp
@@ -0,0 +1,214 @@
+/*
+ RGP-Log.cpp
+
+ Created by Ralph-Gordon Paul on 08.07.13.
+ Copyright (c) 2013 Ralph-Gordon Paul. All rights reserved.
+
+ -------------------------------------------------------------------------------
+ The MIT License (MIT)
+ 
+ Copyright (c) 2014 Ralph-Gordon Paul
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ -------------------------------------------------------------------------------
+*/
+
+
+#include "RGP-Log.h"
+#include <iostream> // cout / cerr
+#include <fstream>  // file writing
+#include <ctime>    // adding date / time to log file
+#include <cstring>  // strerror
+
+using namespace rgp;
+
+// init instance to nullptr
+Log *Log::_sharedInstance = nullptr;
+
+Log *Log::sharedLog() {
+    
+    // if first used -> alloc instance
+    if (_sharedInstance == nullptr) {
+        _sharedInstance = new Log();
+    }
+    
+    return _sharedInstance;
+}
+
+// verbose level 1 print
+void Log::printv(const std::string text)
+{
+    if (logLevel >= 1) {
+        print(text);
+    }
+}
+
+// normal print
+void Log::print(const std::string text)
+{
+    _cout_mutex.lock();
+    
+    // use log file if possible
+    if (_hasLogfile) {
+        
+        // open log file
+        std::ofstream file;
+        file.open(_logFilePath, std::ios::app);
+        
+        // write output to file
+        if (file.is_open()) {
+            
+            // add time to file
+            time_t timestamp = time(NULL); // get time now
+            tm *current_time = localtime( &timestamp );
+            file
+            << (current_time->tm_year + 1900) << '-'
+            << (current_time->tm_mon + 1) << '-'
+            <<  current_time->tm_mday << ' '
+            <<  current_time->tm_hour << ':'
+            <<  current_time->tm_min << ':'
+            <<  current_time->tm_sec << ' ';
+            
+            // append text
+            file << text << std::endl;
+        }
+        
+        // close log file
+        file.close();
+        
+    } else {
+        
+        // output to stdout
+        std::cout << text << std::endl << std::flush;
+    }
+    
+    _cout_mutex.unlock();
+}
+
+// outputs text and reads line from stdin
+std::string Log::getline( const std::string text )
+{
+    char line_c[1024];
+    
+    _cout_mutex.lock();
+    
+    std::cout << text << std::flush;
+    std::cin.getline(line_c, 1024);
+    
+    _cout_mutex.unlock();
+    
+    return std::string { line_c };
+}
+
+// outputs text and reads one character from stdin
+char Log::getc( const std::string text )
+{
+    char c = ' ';
+    
+    _cout_mutex.lock();
+    
+    std::cout << text << std::flush;
+    std::cin >> c;
+    
+    _cout_mutex.unlock();
+    
+    return c;
+}
+
+// error print
+void Log::error(const std::string text)
+{
+    _cerr_mutex.lock();
+    
+    // use log file if possible
+    if (_hasErrorfile) {
+        
+        // open log file
+        std::ofstream file;
+        file.open(_errorFilePath, std::ios::app);
+        
+        // write output to file
+        if (file.is_open()) {
+            
+            // add time to file
+            time_t timestamp = time(NULL); // get time now
+            tm *current_time = localtime( &timestamp );
+            file
+            << (current_time->tm_year + 1900) << '-'
+            << (current_time->tm_mon + 1) << '-'
+            <<  current_time->tm_mday << ' '
+            <<  current_time->tm_hour << ':'
+            <<  current_time->tm_min << ':'
+            <<  current_time->tm_sec << ' ';
+            
+            // append text
+            file << text << std::endl;
+        }
+        
+        // close log file
+        file.close();
+        
+    } else {
+        
+        // output to stderr
+        std::cerr << text << std::endl;
+    }
+    
+    _cerr_mutex.unlock();
+}
+
+// error print with error number (errno)
+void Log::errorWithErrno(const std::string text, int err)
+{
+    // create error string with text + errno
+    std::string error_string { text };
+    error_string += " ";
+    error_string += strerror(err);
+    
+    // output error with errno error string
+    error( error_string );
+}
+
+// using log file for print
+void Log::useLogfile( const std::string filePath )
+{
+    _logFilePath = filePath;
+    
+    // check if file is usable
+    std::ofstream file;
+    file.open(_logFilePath, std::ios::app);
+    if (file.is_open()) {
+        _hasLogfile = true;
+    }
+    file.close();
+}
+
+// using error file
+void Log::useErrorfile( const std::string filePath )
+{
+    _errorFilePath = filePath;
+    
+    // check if file is usable
+    std::ofstream file;
+    file.open(_errorFilePath, std::ios::app);
+    if (file.is_open()) {
+        _hasErrorfile = true;
+    }
+    file.close();
+}
---
+++ RGP-Log.h
@@ -0,0 +1,136 @@
+/*
+ RGP-Log.h
+
+ Created by Ralph-Gordon Paul on 08.07.13.
+ Copyright (c) 2013 Ralph-Gordon Paul. All rights reserved.
+
+ A Singleton Log Class to use for coordinated multithreaded logging
+ (output or logfile)
+
+ -------------------------------------------------------------------------------
+ The MIT License (MIT)
+ 
+ Copyright (c) 2014 Ralph-Gordon Paul
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ -------------------------------------------------------------------------------
+*/
+
+#ifndef __RGP__Log__
+#define __RGP__Log__
+
+#include <mutex>
+#include <atomic>
+#include <string>
+
+// hide debug information (and string data) for better security (less information for an attacker)
+#ifdef DEBUG
+#define RGPLOG(xx) rgp::Log::sharedLog()->print(xx)
+#else
+#define RGPLOG(xx) ((void)0)
+#endif // DEBUG
+
+// hide debug information (and string data) for better security (less information for an attacker)
+// verbose version of RGPLOG
+#ifdef DEBUG
+#define RGPLOGV(xx) rgp::Log::sharedLog()->printv(xx)
+#else
+#define RGPLOGV(xx) ((void)0)
+#endif // DEBUG
+
+// hide debug information (and function name) for better security (less information for an attacker)
+#ifdef DEBUG
+#define RGPLOGMETHODNAME() RGPLOG(__PRETTY_FUNCTION__)
+#else
+#define RGPLOGMETHODNAME() ((void)0)
+#endif // DEBUG
+
+// log errno
+#ifdef DEBUG
+#define RGPLOG_ERRNO(xx,yy) rgp::Log::sharedLog()->errorWithErrno(xx, yy)
+#else
+#define RGPLOG_ERRNO(xx,yy) ((void)0)
+#endif // DEBUG
+
+#ifdef DEBUG
+#define RGPLOG_ERROR(xx) rgp::Log::sharedLog()->error(xx)
+#else
+#define RGPLOG_ERROR(xx) ((void)0)
+#endif // DEBUG
+
+namespace rgp {
+    
+    class Log {
+        
+    public:
+        // log level specifies verbose mode
+        // logLevel = 0 -> normal mode
+        // logLevel = 1 -> verbose mode
+        std::atomic<int> logLevel { 0 };
+        
+        // singleton
+        static Log *sharedLog();
+        
+        void printv(const std::string text);  // verbose print
+        void print(const std::string text);   // normal print
+        
+        std::string getline( const std::string text ); // outputs text and reads line from stdin
+        char getc(const std::string text); // outputs text and reads one character from stdin
+        
+        void error(const std::string text);                   // error print
+        void errorWithErrno( const std::string text, int err); // print errno
+        
+        void useLogfile( const std::string filePath );  // using log file for print
+        void useErrorfile( const std::string filePath );// using error file for error print
+        
+    private:
+        Log() {}                // disallow constructor from outside
+        Log(const Log &log) = delete;    // disallow copy constructor
+        Log &operator = (Log const &) = delete;
+        
+        static Log *_sharedInstance;   // instance of singleton
+        
+        std::mutex _cout_mutex;  // protect cout to make it thread-safe
+        std::mutex _cerr_mutex;  // protect cerr to make it thread-safe
+        
+        // using log file
+        std::atomic<bool> _hasLogfile { false };
+        std::string _logFilePath { "" };
+        
+        // using error file
+        std::atomic<bool> _hasErrorfile { false };
+        std::string _errorFilePath { "" };
+    };
+    
+    /** Exception Class **/
+    class LogException : std::exception {
+        
+    public:
+        LogException() {};
+        LogException(std::string exception) : _exceptionString(exception) {};
+        
+        std::string what() { return _exceptionString; };
+        virtual const char *what() const throw() { return _exceptionString.c_str(); };
+        
+    private:
+        std::string _exceptionString { "Unknown Error" };
+    };
+}
+
+#endif // /* defined(__RGP__Log__) */
